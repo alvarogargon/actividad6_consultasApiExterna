98755fe9fdcb61edb810b28ec87b923eda9c56b8
Alvaro <agar2059@gmail.com>
2025-03-17 17:43:56 +0100

    Cambiamos el nombre de IPaginas para ponerlko como IResponse por seguir las clases de Juanan

--- src/app/interfaces/iresponse.interface.ts
+++ src/app/interfaces/iresponse.interface.ts
@@ -1,18 +1,13 @@
 
 import { IUsuario } from "./iusuario.interface";
 
-
 export interface IResponse {
-    items: IUsuario[]
-    paginas: IPaginas
-}
-    
-    
-export interface IPaginas {
-    
     page: number;
     per_page: number;
     total: number;
     total_pages: number;
+    results: IUsuario[];
+
 }
     
+
