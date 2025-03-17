a9f54d9990af2ec8643e6d5748712b267b112d3b
Alvaro <agar2059@gmail.com>
2025-03-17 18:08:50 +0100

    En el servicio, creamos los metodos getAll, getById y delete inyectando la api para que nos devuelva los usuarios

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IResponse } from '../interfaces/iresponse.interface';
+import { lastValueFrom } from 'rxjs';
+import { IUsuario } from '../interfaces/iusuario.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  private httpClient = inject(HttpClient)
+  private baseUrl: string = "https://peticiones.online/api/users"
 
-  constructor() { }
+  getAll(page: number =0): Promise<IResponse> {
+    let url = (page === 0) ? this.baseUrl : this.baseUrl + `?page = ${page}`
+    return lastValueFrom(this.httpClient.get<IResponse>(url))
+  }
+
+  getByID(id: string): Promise<IUsuario> {
+    return lastValueFrom(this.httpClient.get<IUsuario>(`${this.baseUrl}/${id}`))
+  }
+
+  delete(id: string): Promise<IUsuario> {
+    return lastValueFrom(this.httpClient.delete<IUsuario>(`${this.baseUrl}/${id}`))
+  }
+
+  constructor() { 
+  }
 }
