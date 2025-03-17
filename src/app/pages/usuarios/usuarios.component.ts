f5b34d5c75135e9a487f9437d084105546c2e3fc
Alvaro <agar2059@gmail.com>
2025-03-17 18:18:04 +0100

    Creamos en el usuarios.component.ts los puntos para cargar los usuarios correctamente y los pasos para avanzar y retroceder de pagina

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUsuario } from '../../interfaces/iusuario.interface';
+import { UsuariosService } from '../../services/usuarios.service';
+import { IResponse } from '../../interfaces/iresponse.interface';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrl: './usuarios.component.css'
 })
 export class UsuariosComponent {
+
+  misUsuarios: IUsuario[] = [];
+  usuariosService = inject(UsuariosService)
+  page: number = 0;
+  siguientePagina: number = 0;
+  anteriorPagina: number = 0;
+
+
+  async ngOnInit() {
+    this.listaUsuarios()
+  }
+
+  async siguiente() {
+    this.listaUsuarios(this.siguientePagina)
+  }
+
+  async anterior() {
+    this.listaUsuarios(this.anteriorPagina)
+  }
+
+  async listaUsuarios(page: number = 0) {
+    try {
+      let response: IResponse = await this.usuariosService.getAll(page);
+      this.page = response.page;
+      this.siguientePagina = response.page == response.total_pages ? 0 : response.page + 1;
+      this.anteriorPagina = response.page == 1 ? 0 : response.page -1;
+      this.misUsuarios = response.results;
+    } catch(error){
+
+    }
+  }
 
 }
