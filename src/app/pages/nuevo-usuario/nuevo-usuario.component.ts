b3af596b8e73bb46f476188003161ab24accbe38
Alvaro <agar2059@gmail.com>
2025-03-18 12:01:07 +0100

    Creamos en el ts los validadores de todos los datos que hay que introducir

--- src/app/pages/nuevo-usuario/nuevo-usuario.component.ts
+++ src/app/pages/nuevo-usuario/nuevo-usuario.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-usuario',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './nuevo-usuario.component.html',
   styleUrl: './nuevo-usuario.component.css'
 })
 export class NuevoUsuarioComponent {
+
+  userForm: FormGroup;
+
+  constructor() {
+    this.userForm = new FormGroup({
+      nombre: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      apellido: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      username: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      email: new FormControl("", [
+        Validators.required,
+        Validators.pattern(/^\w+\@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
+      ]),
+      image: new FormControl("", [
+        Validators.required,
+        Validators.pattern(/^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/)
+      ]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(16)
+      ])
+    })
+  }
 
 }
