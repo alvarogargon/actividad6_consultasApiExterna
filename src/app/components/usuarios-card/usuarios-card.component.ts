7e4dfdb76e3b9439bca5328ca30204be1d0da136
Alvaro <agar2059@gmail.com>
2025-03-18 15:10:33 +0100

    Instalamos de nuevo ngxSonner toaster porque el dia anterior nos dio problemas y vamos a intentar hacerlo desde cero

--- src/app/components/usuarios-card/usuarios-card.component.ts
+++ src/app/components/usuarios-card/usuarios-card.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IUsuario } from '../../interfaces/iusuario.interface';
 
 @Component({
   selector: 'app-usuarios-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './usuarios-card.component.html',
   styleUrl: './usuarios-card.component.css'
 })
 export class UsuariosCardComponent {
+  @Input() miUsuario!: IUsuario;
 
 }
