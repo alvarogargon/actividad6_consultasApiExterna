6886f2d79e11c541cea6c4e31144408a53a1a334
Alvaro <agar2059@gmail.com>
2025-03-18 11:40:17 +0100

    Creamos y personalizamos el componente de error 404 llamado C404 para que sea funcional y personalizado

--- src/app/components/c404/c404.component.ts
+++ src/app/components/c404/c404.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-c404',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './c404.component.html',
   styleUrl: './c404.component.css'
 })
