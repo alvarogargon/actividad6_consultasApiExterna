4148e54c9c4c734fb12a27756647c9d629ec8de8
Alvaro <agar2059@gmail.com>
2025-03-19 16:08:59 +0100

    Cargamos el routerLink en el ts

--- src/app/pages/usuario-vista/usuario-vista.component.ts
+++ src/app/pages/usuario-vista/usuario-vista.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, Input } from '@angular/core';
 import { UsuariosService } from '../../services/usuarios.service';
 import { IUsuario } from '../../interfaces/iusuario.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-vista',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './usuario-vista.component.html',
   styleUrl: './usuario-vista.component.css'
 })
